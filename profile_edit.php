68f57bbe4c827ded734e66bcff0153fcd13579d3
Aditya Alvari <aditya.alvari28@gmail.com>
2019-04-06 10:32:24 +0700

    Changed title

--- profile_edit.php
+++ profile_edit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Edit Profile</title>
     <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/darkly/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link href="css/global.css" rel=stylesheet>
     <link href="css/profile_edit.css" rel=stylesheet>
