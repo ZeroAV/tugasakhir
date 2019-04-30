6a339c822573ca99e5986ca4000f4ed9c2565c90
rizkyrevaldy <aldyrev30@gmail.com>
2019-04-30 08:44:06 +0700

    login process

---
+++ login_process.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include_once('connect.php');
+if(!isset($_POST['username'])){
+    header('Location:login.php');
+    die();
+}
+extract($_POST);
+$sql="SELECT * FROM user WHERE username='$username'";
+$result=$mysqli->query($sql);
+if($result->num_rows>0){
+    $row=$result->fetch_assoc();
+    if($row['password']==$password){
+    
+    foreach($row as $key=>$value){
+        $_SESSION["$key"]=$value;
+    }
+    header('Location:index.php');
+    }
+    else{
+        header('Location:login.php?wpassword=1');
+    }
+    
+}
+else{
+    header('Location:login.php?wusername=1');
+}
+
+
+?>
