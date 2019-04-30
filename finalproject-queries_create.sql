23673afffb6702fca44ce263ec829579057786a0
Av <aditya.alvari28@gmail.com>
2019-04-30 08:02:28 +0700

    Updated create and insert queries

--- finalproject-queries_create.sql
+++ finalproject-queries_create.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.5.5-10.1.37-MariaDB)
 # Database: finalproject
-# Generation Time: 2019-04-26 4:44:35 PM +0000
+# Generation Time: 2019-04-30 1:01:47 AM +0000
 # ************************************************************
 
 
@@ -20,6 +20,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+# Dump of table admin
+# ------------------------------------------------------------
+
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `nama` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(20) NOT NULL DEFAULT '',
+  `gender` char(1) NOT NULL DEFAULT '',
+  `bio` longtext,
+  PRIMARY KEY (`username`),
+  CONSTRAINT `fk_admin-uname` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table post
+# ------------------------------------------------------------
+
+CREATE TABLE `post` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `thread_id` int(10) unsigned NOT NULL,
+  `content` longtext NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_edited` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_post-creator` (`username`),
+  KEY `fk_thread-of-post` (`thread_id`),
+  CONSTRAINT `fk_post-creator` FOREIGN KEY (`username`) REFERENCES `user` (`username`),
+  CONSTRAINT `fk_thread-of-post` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`thread_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
 # Dump of table thread
 # ------------------------------------------------------------
 
@@ -29,11 +63,24 @@ CREATE TABLE `thread` (
   `title` varchar(50) NOT NULL DEFAULT '',
   `date_created` datetime NOT NULL,
   `date_last_edited` datetime NOT NULL,
-  `tag` varchar(50) DEFAULT '',
   PRIMARY KEY (`thread_id`),
   KEY `fk_thread-creator` (`username`),
   CONSTRAINT `fk_thread-creator` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+CREATE TABLE `user` (
+  `username` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `gender` char(1) NOT NULL DEFAULT '',
+  `bio` longtext,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
