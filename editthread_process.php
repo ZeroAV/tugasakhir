fc4f8521e4e7662c2aad19ac843cd7c5d97c9c53
Av <aditya.alvari28@gmail.com>
2019-05-05 18:45:09 +0700

    Edit thread functional

--- editthread_process.php
+++ editthread_process.php
@@ -1,12 +1,14 @@
 <?php include "connect.php";
+extract($_POST);
 $id=$_GET['thread'];
-$pid=mysqli_query($mysqli,"SELECT id FROM post WHERE thread_id=$id LIMIT 1");
+$pid=mysqli_query($mysqli,"SELECT id FROM post WHERE thread_id=$id ORDER BY id ASC LIMIT 1");
+$pidg=mysqli_fetch_assoc($pid);
 $title=$_POST['title'];
 $content=$_POST['content'];
-$date=mktime();
-$query=mysqli_query($mysqli, "UPDATE post SET content=$content, date_edited=$date WHERE id=$pid");
+$date=date("Y-m-d H:i:s");
+$query=mysqli_query($mysqli, "UPDATE thread SET title='$title', date_last_edited='$date' WHERE thread_id='$id'");
 if($query){
-    mysqli_query($mysqli, "UPDATE thread SET title=$title, date_last_edited=$date WHERE thread_id=$id");
+    mysqli_query($mysqli, "UPDATE post SET content='$content', date_edited='$date' WHERE id='$pidg[id]'");
     header("location: home_loggedin.php");
 }else{
     echo "<script>alert('Edit failed!').delay(2000);</script>";
